0758fdd39dde627c56c84d7e2ac32849eba2e40f
Rodrigo Lima <drigo.lima1987@gmail.com>
2023-04-15 16:43:02 -0300

    primeira entrega ac1

--- gestao_de_clientes/urls.py
+++ gestao_de_clientes/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from clientes import urls as clientes_urls
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from home import urls as home_urls
+
 
 urlpatterns = [
+    path('', include(home_urls)),
+    path('clientes/', include(clientes_urls)),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
